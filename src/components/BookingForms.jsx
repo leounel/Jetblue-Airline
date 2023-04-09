ac25c6eb57bf50fad8139e2c06aaaf96eb46641a
leonel <leounel13@gmail.com>
2023-04-09 17:13:44 -0300

    added destinations, dates and class inputs

--- src/components/BookingForms.jsx
+++ src/components/BookingForms.jsx
@@ -4,14 +4,38 @@ const BookingForms = () => {
         <h2>1. <br/>Reserva tu ticket.</h2>
         <form className="credentials-form">
             <section className="flight-type-buttons">
-                <button type="button">Solo ida</button>
+                <button style={{color:"#676767"}} type="button">Solo ida</button>
                 <button type="button">Ida y vuelta</button>
             </section>
-            <section className="destination-inputs">
-                <label>ORIGEN</label>
-                <input placeholder="hola"></input>
-                <label>DESTINO</label>
-                <input placeholder="¿Hacia dónde?"></input>
+            <section className="destination-inputs-container">
+                <div className="destination-input">
+                    <label>ORIGEN</label>
+                    <input placeholder="¿De dónde?"/>
+                </div>
+                <div className="destination-input">
+                    <label>DESTINO</label>
+                    <input placeholder="¿Hacia dónde?"/>
+                </div>
+            </section>
+            <section className="dates-container">
+                <span>FECHAS</span>
+                <div className="dates-input-container">
+                    <div className="date-departure">
+                        <label htmlFor="">IDA</label>
+                        <input placeholder="May 09, 2023"></input>
+                    </div>
+                    <div className="date-return">
+                        <label htmlFor="">VUELTA</label>
+                        <input placeholder="May 17, 2023"></input>
+                    </div>
+                </div>
+            </section>
+            <section className="class-container">
+                <span>CLASE</span>
+                <select>
+                    <option>Económica</option>
+                    <option>Ejecutiva</option>
+                </select>
             </section>
         </form>
     </main>
